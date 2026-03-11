ae31f326e40a5bae660881cf425dee58d5944330
Renzo Vizarreta <rvizarreta14@gmail.com>
2026-03-11 17:07:20 -0500

    new test

--- selection/src/main.cc
+++ selection/src/main.cc
@@ -404,8 +404,6 @@ int main(int argc, char * argv[])
                                                 bool passes_reco = std::all_of(tree_reco_fns.begin(), tree_reco_fns.end(), [&](auto & f){ return f(sr->dlp[match_id]); });
                                                 if(!passes_reco) continue;
                                             }
-                                            bool passes_mctruth = (i.nu_id < 0 || (size_t)i.nu_id >= sr->mc.nu.size()) || std::all_of(tree_mctruth_fns.begin(), tree_mctruth_fns.end(), [&](auto & f){ return f(sr->mc.nu[i.nu_id]); });
-                                            if(!passes_mctruth) continue;
 
                                             bool matched = false;
                                             for(const auto & [category, cuts_pair] : category_cut_functions)
